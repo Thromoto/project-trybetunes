d303f922c6765a2653a35b3324e5c6605882bac8
Thiago Omoto <thi.omoto@gmail.com>
2022-10-04 16:45:09 -0300

    requisito 3 done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      usuario: '',
+    };
+  }
+
+  async componentDidMount() {
+    const usuario = await getUser();
+    // console.log(usuario);
+    this.setState({ usuario });
+  }
+
   render() {
+    const { usuario } = this.state;
     return (
-      <header data-testid="header-component" />
+      <header data-testid="header-component">
+        { usuario ? (
+          <div data-testid="header-user-name">
+            { usuario.name }
+          </div>)
+          : <Loading /> }
+      </header>
     );
   }
 }
