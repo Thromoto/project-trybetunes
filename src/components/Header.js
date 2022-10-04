067777a71fb75d7e94bd19a9ebcf01bb2923f71a
Thiago Omoto <thi.omoto@gmail.com>
2022-10-04 18:43:02 -0300

    requisito 4 done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -19,13 +20,18 @@ class Header extends React.Component {
   render() {
     const { usuario } = this.state;
     return (
-      <header data-testid="header-component">
-        { usuario ? (
-          <div data-testid="header-user-name">
-            { usuario.name }
-          </div>)
-          : <Loading /> }
-      </header>
+      <>
+        <header data-testid="header-component">
+          { usuario ? (
+            <div data-testid="header-user-name">
+              { usuario.name }
+            </div>)
+            : <Loading /> }
+        </header>
+        <Link to="/search" data-testid="link-to-search">Search</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+        <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+      </>
     );
   }
 }
