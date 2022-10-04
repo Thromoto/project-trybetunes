191a55d35ef5e58a1aa6a5a4991641e3454fe33b
Thiago Omoto <thi.omoto@gmail.com>
2022-10-04 15:51:18 -0300

    requisito 2 done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -12,13 +13,15 @@ class App extends React.Component {
     return (
       <>
         <p>TrybeTunes</p>
-        <Login />
-        <Search />
-        <Album />
-        <Favorites />
-        <Profile />
-        <ProfileEdit />
-        <NotFound />
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
       </>
     );
   }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,76 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      btnSaveDisable: true,
+      loading: false,
+    };
+  }
+
+  enableBtn = () => {
+    const { name } = this.state;
+    const numero = 3;
+    const typedName = name.length >= numero;
+    this.setState({ btnSaveDisable: typedName === false });
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    }, () => this.enableBtn());
+  };
+
+  btnSave = async () => {
+    const { name } = this.state;
+    const { history } = this.props;
+    this.setState({ loading: true });
+    await createUser({ name });
+    history.push('/search');
+  };
+
   render() {
+    const { name, btnSaveDisable, loading } = this.state;
     return (
-      <div data-testid="page-login" />
+      <div>
+        { loading
+          ? <Loading />
+          : (
+            <div data-testid="page-login">
+              <p>Login</p>
+              <label htmlFor="name">
+                <input
+                  type="text"
+                  name="name"
+                  value={ name }
+                  onChange={ this.handleChange }
+                  data-testid="login-name-input"
+                />
+              </label>
+              <button
+                type="button"
+                data-testid="login-submit-button"
+                disabled={ btnSaveDisable }
+                onClick={ this.btnSave }
+              >
+                Entrar
+              </button>
+            </div>
+          )}
+      </div>
     );
   }
 }
+
+Login.propTypes = ({
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}).isRequired;
 
 export default Login;
